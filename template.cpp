50a61faf46226b1030ecf8b8f7e1138a6a825669
Mateo <mateovaldivia3000@gmail.com>
2023-02-24 15:46:49 -0400

    added c++ template

---
+++ template.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <bits/stdc++.h>
+#define fore(i, a, b) for(int i = a; i < b; i++)
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define lcm(a,b) (a/__gcd(a,b))*b
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define F first
+#define S second
+#define int long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+signed main()
+{
+    fast;
+    
+    return 0;
+}  
+// MOL GORUYI MOLA UBI HE BEÁSE
+// DUL YUNGE HANAMAN GOLA Yes or Yes?
