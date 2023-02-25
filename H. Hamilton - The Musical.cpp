bcba82eb324406ef8246be72e4f3156def6a804c
mateoo <mateovaldivia3000@gmail.com>
2023-02-25 00:14:40 -0400

    min cost ez

---
+++ H. Hamilton - The Musical.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <bits/stdc++.h>
+#define fore(i, a, b) for(int i = a; i < b; i++)
+#define forn(i, n) for(int i = 0; i < n; i++)
+#define lcm(a,b) (a/__gcd(a,b))*b
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define pb push_back
+#define F first
+#define S second
+#define int long long
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N = 500 + 9;
+
+
+//Works for both directed, undirected and with negative cost too
+//doesn't work for negative cycles
+//for undirected edges just make the directed flag false
+//Complexity: O(min(E^2 *V log V, E logV * flow))
+using T = long long;
+const T inf = 1LL << 61;
+struct MCMF {
+  struct edge {
+    int u, v;
+    T cap, cost;
+    int id;
+    edge(int _u, int _v, T _cap, T _cost, int _id) {
+      u = _u;
+      v = _v;
+      cap = _cap;
+      cost = _cost;
+      id = _id;
+    }
+  };
+  int n, s, t, mxid;
+  T flow, cost;
+  vector<vector<int>> g;
+  vector<edge> e;
+  vector<T> d, potential, flow_through;
+  vector<int> par;
+  bool neg;
+  MCMF() {}
+  MCMF(int _n) { // 0-based indexing
+    n = _n + 10;
+    g.assign(n, vector<int> ());
+    neg = false;
+    mxid = 0;
+  }
+  void add_edge(int u, int v, T cap, T cost, int id = -1, bool directed = true) {
+    if(cost < 0) neg = true;
+    g[u].push_back(e.size());
+    e.push_back(edge(u, v, cap, cost, id));
+    g[v].push_back(e.size());
+    e.push_back(edge(v, u, 0, -cost, -1));
+    mxid = max(mxid, id);
+    if(!directed) add_edge(v, u, cap, cost, id + N, true); // tracks the reverse edges for undirected graphs
+  }
+  bool dijkstra() {
+    par.assign(n, -1);
+    d.assign(n, inf);
+    priority_queue<pair<T, T>, vector<pair<T, T>>, greater<pair<T, T>> > q;
+    d[s] = 0;
+    q.push(pair<T, T>(0, s));
+    while (!q.empty()) {
+      int u = q.top().second;
+      T nw = q.top().first;
+      q.pop();
+      if(nw != d[u]) continue;
+      for (int i = 0; i < (int)g[u].size(); i++) {
+        int id = g[u][i];
+        int v = e[id].v;
+        T cap = e[id].cap;
+        T w = e[id].cost + potential[u] - potential[v];
+        if (d[u] + w < d[v] && cap > 0) {
+          d[v] = d[u] + w;
+          par[v] = id;
+          q.push(pair<T, T>(d[v], v));
+        }
+      }
+    }
+    for (int i = 0; i < n; i++) { // update potential
+      if(d[i] < inf) potential[i] += d[i];
+    }
+    return d[t] != inf;
+  }
+  T send_flow(int v, T cur) {
+    if(par[v] == -1) return cur;
+    int id = par[v];
+    int u = e[id].u;
+    T w = e[id].cost;
+    T f = send_flow(u, min(cur, e[id].cap));
+    cost += f * w;
+    e[id].cap -= f;
+    e[id ^ 1].cap += f;
+    return f;
+  }
+  //returns {maxflow, mincost}
+  pair<T, T> solve(int _s, int _t, T goal = inf) {
+    s = _s;
+    t = _t;
+    flow = 0, cost = 0;
+    potential.assign(n, 0);
+    if (neg) {
+      // run Bellman-Ford to find starting potential
+      d.assign(n, inf);
+      for (int i = 0, relax = true; i < n && relax; i++) {
+        for (int u = 0; u < n; u++) {
+          for (int k = 0; k < (int)g[u].size(); k++) {
+            int id = g[u][k];
+            int v = e[id].v;
+            T cap = e[id].cap, w = e[id].cost;
+            if (d[v] > d[u] + w && cap > 0) {
+              d[v] = d[u] + w;
+              relax = true;
+            }
+          }
+        }
+      }
+      for(int i = 0; i < n; i++) if(d[i] < inf) potential[i] = d[i];
+    }
+    while (flow < goal && dijkstra()) flow += send_flow(t, goal - flow);
+    flow_through.assign(mxid + 10, 0);
+    for (int u = 0; u < n; u++) {
+      for (auto v : g[u]) {
+        if (e[v].id >= 0) flow_through[e[v].id] = e[v ^ 1].cap;
+      }
+    }
+    return make_pair(flow, cost);
+  }
+};
+int M[505][505];
+signed main(){
+    fast;
+    int n;
+    cin>>n;
+    // memset(M,0,sizeof(M));
+    MCMF F(2*n+5);
+    //mi cost matching
+    for(int i=1;i<=n;i++){
+        if(i%2){
+            F.add_edge(0,i,1,0);
+            // cout<<0<<" -> "<<i<<endl;
+        }
+        for(int l=1;l<=n;l++){
+            cin>>M[i][l];
+        }
+    }
+    for(int i=1;i<=n;i+=2){
+        F.add_edge(i+n,2*n+1,1,0);
+        for(int l=1;l<=n;l+=2){
+            //quiero poner i en la posicion l
+            int precio=M[i][l-1]+M[i][l+1];  
+            F.add_edge(i,l+n,1,precio);
+            // cout<<i<<" -> "<<l+n<<endl;
+        }
+    }
+    ii res = F.solve(0,2*n+1);
+    cout<<res.S<<endl;
+
+    
+    return 0;
+}
+// MOL GORUYI MOLA UBI HE BEÁSE
+// DUL YUNGE HANAMAN GOLA YES OR YES?
+// https://codeforces.com/gym/103640/problem/H
